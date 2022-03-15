92e8564b049f5db41352308bddea24be0f47d528
Kenji Brameld <kenjibrameld@gmail.com>
2022-03-15 15:43:48 +0900

    fix flake8
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- ipm_library/ipm_library/exceptions.py
+++ ipm_library/ipm_library/exceptions.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class NoIntersectionError(Exception):
     """Rased if a point is not able to be projected onto the plane."""
 

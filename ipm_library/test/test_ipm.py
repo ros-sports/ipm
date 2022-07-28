351c00d28daa9e99e44941396e6f14da5ebe46be
Kenji Brameld <kenjibrameld@gmail.com>
2022-07-28 04:01:02 +0000

    add docstring to test
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- ipm_library/test/test_ipm.py
+++ ipm_library/test/test_ipm.py
@@ -309,12 +309,14 @@ def test_ipm_project_points():
 
 
 def test_project_point_invalid_plane_exception():
+    """Check InvalidPlaneException is raised if a plane is invalid, i.e. a=b=c=0."""
     ipm = IPM(tf2.Buffer(), CameraInfo())
     with pytest.raises(InvalidPlaneException):
         ipm.project_point(PlaneStamped(), Point2DStamped())
 
 
 def test_project_points_invalid_plane_exception():
+    """Check InvalidPlaneException is raised if a plane is invalid, i.e. a=b=c=0."""
     ipm = IPM(tf2.Buffer(), CameraInfo())
     with pytest.raises(InvalidPlaneException):
         ipm.project_points(PlaneStamped(), np.array([]), Header())

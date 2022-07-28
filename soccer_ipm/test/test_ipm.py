1cfbf1534e1c739ca8ec5b3a09f3fa7a55776d75
Florian Vahl <florian@flova.de>
2022-07-28 08:10:13 +0000

    Add test abstraction

--- soccer_ipm/test/test_ipm.py
+++ soccer_ipm/test/test_ipm.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from geometry_msgs.msg import TransformStamped
 import numpy as np
@@ -20,7 +20,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CameraInfo
 from soccer_ipm.soccer_ipm import SoccerIPM
-from soccer_vision_2d_msgs.msg import Ball, BallArray
+from soccer_vision_2d_msgs.msg import Ball, BallArray, GoalpostArray, RobotArray
 import soccer_vision_3d_msgs.msg as sv3dm
 from soccer_vision_attribute_msgs.msg import Confidence
 from std_msgs.msg import Header
@@ -38,23 +38,29 @@ camera_info = CameraInfo(
         k=[1338.64532, 0., 1024.0, 0., 1337.89746, 768.0, 0., 0., 1.])
 
 
-def test_ipm_empty_ball():
+def standard_ipm_test_case(
+        input_msg_type: type,
+        input_topic: str,
+        input_msg: Union[BallArray, GoalpostArray, RobotArray],
+        output_msg_type: type,
+        output_topic: str) -> Union[
+            sv3dm.BallArray, sv3dm.GoalpostArray, sv3dm.RobotArray]:
     # Init ros
     rclpy.init()
     # Create IPM node
     node = SoccerIPM()
     # Create test node which comunicates with the IPM node
-    test_node = Node('test')
+    test_node = Node('test_handler')
     # Create publishers to send data to the IPM node
     ball_pub = test_node.create_publisher(
-        BallArray, 'balls_in_image', 10)
+        input_msg_type, input_topic, 10)
     camera_info_pub = test_node.create_publisher(
         CameraInfo, 'camera_info', 10)
     tf_pub = test_node.create_publisher(
         TFMessage, 'tf', 10)
 
     # Create a shared reference to the recived message in the local scope
-    received_msg: List[Optional[sv3dm.BallArray]] = [None]
+    received_msg: List[Optional[output_msg_type]] = [None]
 
     # Create a callback with sets this reference
     def callback(msg):
@@ -62,73 +68,7 @@ def test_ipm_empty_ball():
 
     # Subscribe to IPM results
     test_node.create_subscription(
-        sv3dm.BallArray, 'balls_relative', callback, 10)
-
-    # Create header message for the current time stamp in the camera frame
-    header = Header(
-        stamp=node.get_clock().now().to_msg(),
-        frame_id="camera_optical_frame")
-
-    # Publish a dummy transform from the camera to the base_footprint frame
-    tf_pub.publish(TFMessage(
-        transforms=[
-            TransformStamped(
-                header=header,
-                child_frame_id="base_footprint",
-            )
-        ]
-    ))
-    # Spin the ipm to process the new data
-    rclpy.spin_once(node, timeout_sec=0.1)
-
-    # Send camera info message to the IPM
-    camera_info.header.stamp = header.stamp
-    camera_info_pub.publish(camera_info)
-    # Spin the IPM to process the new data
-    rclpy.spin_once(node, timeout_sec=0.1)
-
-    # Send empty ball array to the IPM
-    ball_pub.publish(BallArray())
-    # Spin the IPM to process the new data
-    rclpy.spin_once(node, timeout_sec=0.1)
-
-    # Spin the test__node to recive the results from the IPM
-    rclpy.spin_once(test_node, timeout_sec=0.1)
-
-    # Assert that we recived a message
-    assert received_msg[0] is not None
-
-    # Clean shutdown of the nodes
-    rclpy.shutdown()
-    node.destroy_node()
-    test_node.destroy_node()
-
-
-def test_ipm_ball():
-    # Init ros
-    rclpy.init()
-    # Create IPM node
-    node = SoccerIPM()
-    # Create test node which comunicates with the IPM node
-    test_node = Node('test')
-    # Create publishers to send data to the IPM node
-    ball_pub = test_node.create_publisher(
-        BallArray, 'balls_in_image', 10)
-    camera_info_pub = test_node.create_publisher(
-        CameraInfo, 'camera_info', 10)
-    tf_pub = test_node.create_publisher(
-        TFMessage, 'tf', 10)
-
-    # Create a shared reference to the recived message in the local scope
-    received_msg: List[Optional[sv3dm.BallArray]] = [None]
-
-    # Create a callback with sets this reference
-    def callback(msg):
-        received_msg[0] = msg
-
-    # Subscribe to IPM results
-    test_node.create_subscription(
-        sv3dm.BallArray, 'balls_relative', callback, 10)
+        output_msg_type, output_topic, callback, 10)
 
     # Create header message for the current time stamp in the camera frame
     header = Header(
@@ -157,17 +97,9 @@ def test_ipm_ball():
     # Spin the IPM to process the new data
     rclpy.spin_once(node, timeout_sec=0.1)
 
-    # Send ball array with one ball to the IPM
-    ball = Ball()
-    ball.center.x = camera_info.width / camera_info.binning_x // 2
-    ball.center.y = camera_info.height / camera_info.binning_y // 2
-    ball.confidence = Confidence(confidence=0.42)
-    ball_pub.publish(BallArray(
-        header=header,
-        balls=[
-            ball
-        ]
-    ))
+    # Send image space detection
+    input_msg.header = header
+    ball_pub.publish(input_msg)
     # Spin the IPM to process the new data
     rclpy.spin_once(node, timeout_sec=0.1)
 
@@ -177,20 +109,47 @@ def test_ipm_ball():
     # Assert that we recived a message
     assert received_msg[0] is not None
 
+    # Clean shutdown of the nodes
+    rclpy.shutdown()
+    node.destroy_node()
+    test_node.destroy_node()
+
+    return received_msg[0], input_msg
+
+
+def test_ipm_empty_ball():
+    standard_ipm_test_case(
+        BallArray,
+        'balls_in_image',
+        BallArray(),
+        sv3dm.BallArray,
+        'balls_relative')
+
+
+def test_ipm_ball():
+    # Create ball detection
+    ball = Ball()
+    ball.center.x = camera_info.width / camera_info.binning_x // 2
+    ball.center.y = camera_info.height / camera_info.binning_y // 2
+    ball.confidence = Confidence(confidence=0.42)
+    ball_detection = BallArray(balls=[ball])
+
+    out, inp = standard_ipm_test_case(
+        BallArray,
+        'balls_in_image',
+        ball_detection,
+        sv3dm.BallArray,
+        'balls_relative')
+
     # Assert that we recived the correct message
-    assert len(received_msg[0].balls) == 1, 'Got too many balls'
-    assert received_msg[0].header.stamp == header.stamp, 'Time stamp got changed by the ipm'
-    assert received_msg[0].header.frame_id == 'base_footprint', \
+    assert len(out.balls) == 1, 'Got too many balls'
+    assert out.header.stamp == inp.header.stamp, 'Time stamp got changed by the ipm'
+    assert out.header.frame_id == 'base_footprint', \
         'Output frame is not "base_footprint"'
-    ball_relative: sv3dm.Ball = received_msg[0].balls[0]
+    ball_relative: sv3dm.Ball = out.balls[0]
     np.testing.assert_allclose(
         ball_relative.confidence.confidence,
         ball.confidence.confidence)
     np.testing.assert_allclose(
         [ball_relative.center.x, ball_relative.center.y, ball_relative.center.z],
         [0.0, 0.0, 0.0])
-
-    # Clean shutdown of the nodes
-    rclpy.shutdown()
-    node.destroy_node()
-    test_node.destroy_node()

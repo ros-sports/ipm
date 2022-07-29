8b4351f94efffa0dfc554d63fdb0b5163eeb6638
Florian Vahl <florian@flova.de>
2022-07-29 10:19:15 +0000

    Fix code so all test are passing

--- ipm_service/ipm_service/ipm.py
+++ ipm_service/ipm_service/ipm.py
@@ -12,17 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import rclpy
-import tf2_ros as tf2
-from rclpy.node import Node
-from rclpy.duration import Duration
-from rclpy.executors import MultiThreadedExecutor
 from ipm_interfaces.srv import MapPoint, MapPointCloud2
 from ipm_library.exceptions import InvalidPlaneException, NoIntersectionError
-from sensor_msgs.msg import CameraInfo
-from sensor_msgs_py.point_cloud2 import read_points_numpy, create_cloud_xyz32
-from std_msgs.msg import Header
 from ipm_library.ipm import IPM
+import rclpy
+from rclpy.duration import Duration
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from sensor_msgs.msg import CameraInfo
+from sensor_msgs_py.point_cloud2 import create_cloud_xyz32, read_points_numpy
+from std_msgs.msg import Header
+import tf2_ros as tf2
 
 
 class IPMService(Node):
@@ -34,10 +34,13 @@ class IPMService(Node):
         # Create ipm library instance
         self.ipm = IPM(self.tf_buffer)
         # Create subs
-        self.camera_info_sub = self.create_subscription(CameraInfo, 'camera_info', self.ipm.set_camera_info)
+        self.camera_info_sub = self.create_subscription(
+            CameraInfo, 'camera_info', self.ipm.set_camera_info, 1)
         # Create services
-        self.point_srv = self.create_service(MapPoint, 'map_point', self.point_mapping_callback)
-        self.point_cloud_srv = self.create_service(MapPointCloud2, 'map_pointcloud2', self.point_cloud_mapping_callback)
+        self.point_srv = self.create_service(
+            MapPoint, 'map_point', self.point_mapping_callback)
+        self.point_cloud_srv = self.create_service(
+            MapPointCloud2, 'map_pointcloud2', self.point_cloud_mapping_callback)
 
     def point_mapping_callback(
             self,
@@ -56,23 +59,22 @@ class IPMService(Node):
             return response
 
         # Map optional marking from "" to None
-        if request.output_frame == "":
+        if request.output_frame.data == "":
             output_frame = None
         else:
-            output_frame = request.output_frame
+            output_frame = request.output_frame.data
 
-        # Project the given point and handle different result scenarios
+        # Maps the given point and handle different result scenarios
         try:
-            response.point = self.ipm.project_point(
+            response.point = self.ipm.map_point(
                 request.plane,
                 request.point,
                 output_frame)
+            response.result = MapPoint.Response.RESULT_SUCCESS
         except NoIntersectionError:
             response.result = MapPoint.Response.RESULT_NO_INTERSECTION
         except InvalidPlaneException:
             response.result = MapPoint.Response.RESULT_INVALID_PLANE
-        finally:
-            response.result = MapPoint.Response.RESULT_SUCCESS
         return response
 
     def point_cloud_mapping_callback(
@@ -92,30 +94,32 @@ class IPMService(Node):
             return response
 
         # Map optional marking from "" to None
-        if request.output_frame == "":
+        if request.output_frame.data == "":
             output_frame = self.ipm.get_camera_info().header.frame_id
         else:
-            output_frame = request.output_frame
+            output_frame = request.output_frame.data
 
-        # Project the given point and handle different result scenarios
+        # Map the given point and handle different result scenarios
         try:
-            projected_points = self.ipm.project_points(
+            mapped_points = self.ipm.map_points(
                 request.plane,
                 read_points_numpy(request.points),
+                request.points.header,
                 output_frame)
+
+            # Convert them into a PointCloud2
+            response.points = create_cloud_xyz32(
+                Header(
+                    stamp=request.points.header.stamp,
+                    frame_id=output_frame),
+                mapped_points)
+
+            response.result = MapPointCloud2.Response.RESULT_SUCCESS
         except NoIntersectionError:
             response.result = MapPointCloud2.Response.RESULT_NO_INTERSECTION
         except InvalidPlaneException:
             response.result = MapPointCloud2.Response.RESULT_INVALID_PLANE
-        finally:
-            response.result = MapPointCloud2.Response.RESULT_SUCCESS
 
-        # Convert them into a PointCloud2
-        response.points = create_cloud_xyz32(
-            Header(
-                stamp=request.plane.stamp,
-                frame_id=output_frame),
-            projected_points)
         return response
 
 

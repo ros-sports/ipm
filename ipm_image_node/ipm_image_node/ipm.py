caecd2cb85c077977ce5901c779c5a81a413641a
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2025-06-17 15:23:39 +0200

    Remove trailing whitespace

--- ipm_image_node/ipm_image_node/ipm.py
+++ ipm_image_node/ipm_image_node/ipm.py
@@ -178,7 +178,7 @@ def main(args=None):
     node = IPMImageNode()
     if fast_tf_buffer_available:
         # If bitbots_tf_buffer is available, we can use the EventsExecutor
-        # which is single threaded, using it without the decoupled bitbots_tf_buffer 
+        # which is single threaded, using it without the decoupled bitbots_tf_buffer
         # would lead to deadlocks.
         ex = EventsExecutor()
     else:
